ee1bf4d374124d079097a8b270447900923b9387
Agung Wirayogi <agungwy@gmail.com>
2015-05-13 17:48:36 +0700

    Revert "Revert "Renew file""
    This reverts commit 073bd2f513c5458b1d74d566a26b35886d50346d.

--- Register_Web/src/RegisterTest.java
+++ Register_Web/src/RegisterTest.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 
-public class test1 {
+public class RegisterTest {
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	static final String DB_URL = "jdbc:mysql://localhost/adprog";
@@ -42,7 +42,7 @@ public class test1 {
 	}
 
 
-	public static void Register(String uname,String pass,int npm) {
+	public static boolean Register(String uname,String pass,int npm) {
 
 		Connection conn = null;
 		Statement stmt = null;
@@ -68,16 +68,19 @@ public class test1 {
 		}catch(SQLException se){
 			//Handle errors for JDBC
 			se.printStackTrace();
+			return false;
 		}catch(Exception e){
 			//Handle errors for Class.forName
 			e.printStackTrace();
 			System.out.println("Do not connect to DB - Error: " + e);
+			return false;
 		}finally{
 			//finally block used to close resources
 			try{
 				if(stmt!=null)
 					conn.close();
 			}catch(SQLException se){
+				return false;
 			}// do nothing
 			try{
 				if(conn!=null){
@@ -85,8 +88,10 @@ public class test1 {
 				}
 			}catch(SQLException se){
 				se.printStackTrace();
+				return false;
 			}//end finally try
 		}//end try
 		System.out.println("Goodbye!");
+		return true;
 	}//end Register
 }
