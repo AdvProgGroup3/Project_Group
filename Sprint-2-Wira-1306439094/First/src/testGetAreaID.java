62aca1305e7067cd7644056480c4a99f0f332e18
Agung Wirayogi <agungwy@gmail.com>
2015-05-17 19:58:12 +0700

    Adding AllTest.java and fixing bugs

--- Sprint-2-Wira-1306439094/First/src/testGetAreaID.java
+++ Sprint-2-Wira-1306439094/First/src/testGetAreaID.java
@@ -8,11 +8,11 @@ public class testGetAreaID {
 	@Test
 	public void test() {
 		System.out.println("test the getAreaID() method");
-		int user_id = 1;
+		int user_id = 5;
 		
 		Control data = new Control(user_id);
 		assertEquals("a01", data.getAreaID());
-		System.out.println("user_id = 1, expected area_id = a01\n");
+		System.out.println("user_id = 5, expected area_id = a01\n");
 	}
 
 }
